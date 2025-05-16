af698ab04d528d87d139fd1003ef5c0f96885827
Harsh Sharma <namastecoding@gmail.com>
2025-05-16 12:29:03 +0530

    auth flow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { ClerkProvider, SignedOut } from "@clerk/nextjs";
-import SignInPage from "./(auth)/sign-in/[[...sign-in]]/page";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -22,7 +21,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider
+      afterSignOutUrl="/"
+      appearance={{ variables: { colorPrimary: "#624cf5" } }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
